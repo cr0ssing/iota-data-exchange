09af1fb825b8a1c9dc3ee29e9de37f5cb7c0a3d7
Robin Lamberti <lamberti@fzi.de>
2020-03-11 17:23:46 +0100

    formatting

--- evaluatePerformance.js
+++ evaluatePerformance.js
@@ -3,62 +3,89 @@ const hash = require('object-hash');
 const util = require('util');
 
 // evalFile('hashFromDatetag-desktop.json');
-evalFile('publish-desktop.json', ['sideKeyCalculation', 'createMessage', 'attachMessage'], ['mwm'])
+evalFile(
+  'publish-desktop.json',
+  ['sideKeyCalculation', 'createMessage', 'attachMessage'],
+  ['mwm']
+);
 
-function evalFile(name, durationFields = ['duration'], groupingFields = ['format']) {
+function evalFile(
+  name,
+  durationFields = ['duration'],
+  groupingFields = ['format']
+) {
   try {
-    const file = fs.readFileSync(name, 'utf8')
+    const file = fs.readFileSync(name, 'utf8');
     const data = JSON.parse(file);
     const map = new Map();
     const hashes = new Map();
+    let result;
     for (let point of data) {
-      const group = groupingFields.map(n => ({[n]: point[n]})).reduce((acc, v) => ({ ...acc, ...v}));
+      const group = groupingFields
+        .map(n => ({ [n]: point[n] }))
+        .reduce((acc, v) => ({ ...acc, ...v }));
       const key = hash(group);
       if (!map.has(key)) {
         map.set(key, []);
         hashes.set(key, group);
       }
-      const stats = durationFields.map(k => ({ [k]: point[k] })).reduce((acc, v) => ({...acc, ...v}));
+      const stats = durationFields
+        .map(k => ({ [k]: point[k] }))
+        .reduce((acc, v) => ({ ...acc, ...v }));
       map.get(key).push(stats);
     }
     const absolute = new Map();
     Array.from(map.entries()).forEach(([key, value]) => {
       const getPoints = name => value.map(p => p[name]);
-      const stats = durationFields.map(k => ({ [k]: {
-        min: Math.min(...getPoints(k)),
-        max: Math.max(...getPoints(k)),
-        avg: getPoints(k).reduce((acc, v) => acc + v) / getPoints(k).length,
-      } })).reduce((acc, v) => ({...acc, ...v}));
+      const stats = durationFields
+        .map(k => ({
+          [k]: {
+            min: Math.min(...getPoints(k)),
+            max: Math.max(...getPoints(k)),
+            avg: getPoints(k).reduce((acc, v) => acc + v) / getPoints(k).length,
+          },
+        }))
+        .reduce((acc, v) => ({ ...acc, ...v }));
 
       absolute.set(hashes.get(key), stats);
     });
-    const relative = new Map();
-    const sums = new Map();
-    const props = ['min', 'max', 'avg']
-    Array.from(absolute.entries()).forEach(([key, value]) => {
-      const groupSums = {};
-      const groupPortions = {}
+    if (durationFields.length > 1) {
+      const relative = new Map();
+      const sums = new Map();
+      const props = ['min', 'max', 'avg'];
+      Array.from(absolute.entries()).forEach(([key, value]) => {
+        const groupSums = {};
+        const groupPortions = {};
 
-      props.forEach(prop => {
-        const sum = durationFields
-          .map(durationName => value[durationName][prop])
-          .reduce((acc, v) => acc + v);
-        groupSums[prop] = sum;
+        props.forEach(prop => {
+          const sum = durationFields
+            .map(durationName => value[durationName][prop])
+            .reduce((acc, v) => acc + v);
+          groupSums[prop] = sum;
 
-        groupPortions[prop] = durationFields
-          .map(durationName => ({ [durationName]: value[durationName][prop] / sum}))
-          .reduce((acc, v) => ({...acc, ...v}));
+          groupPortions[prop] = durationFields
+            .map(durationName => ({
+              [durationName]: value[durationName][prop] / sum,
+            }))
+            .reduce((acc, v) => ({ ...acc, ...v }));
+        });
+        sums.set(key, groupSums);
+        relative.set(key, groupPortions);
+
+        result = {
+          absolute,
+          sums,
+          relative,
+        };
       });
-      sums.set(key, groupSums);
-      relative.set(key, groupPortions);
-    });
-
-    console.log(name + ':', util.inspect({
-      absolute,
-      sums,
-      relative,
-    }, false, null, true /* enable colors */))
+    } else {
+      result = absolute;
+    }
+    console.log(
+      name + ':',
+      util.inspect(result, false, null, true /* enable colors */)
+    );
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
